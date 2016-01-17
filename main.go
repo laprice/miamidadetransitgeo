456da5363c4c746f684c196c285a04b78d4ffec5
Larry Price <larry@industrialintellect.com>
2016-01-17 15:49:56 -0800

    initial version.
    Fetches and parses url and outputs what we need.
    
    Next step is to poke it into a format that's easy for Leaflet to take.

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "net/http"
+import "io/ioutil"
+import "encoding/xml"
+import "log"
+
+type Record struct {
+	BusID            string `xml:"Record>BusID"`
+	BusName          string `xml:"Record>BusName"`
+	Latitude         string `xml:"Record>Latitude"`
+	Longitude        string `xml:"Record>Longitude"`
+	RouteID          string `xml:"Record>RouteID"`
+	TripID           string `xml:"Record>TripID"`
+	Direction        string `xml:"Record>Direction"`
+	ServiceDirection string `xml:"Record>ServiceDirection"`
+	Service          string `xml:"Record>Service"`
+	ServiceName      string `xml:"Record>ServiceName"`
+	TripHeadsign     string `xml:"Record>TripHeadsign"`
+	LocationUpdated  string `xml:"Record>LocationUpdated"`
+}
+
+func main() {
+	log.Println("starting")
+	resp, err := http.Get("http://www.miamidade.gov/transit/WebServices/Buses/?RouteID=1")
+	if err != nil {
+		log.Fatal("error fetching url", err)
+	}
+	log.Println(resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("could not read Response Body")
+	}
+	resp.Body.Close()
+	r := new(Record)
+	err = xml.Unmarshal(body, &r)
+	if err != nil {
+		log.Fatal("Unmarshall failed", err)
+	}
+	log.Println(r.BusName)
+	log.Printf("POINT(%s %s)", r.Longitude, r.Latitude)
+}
